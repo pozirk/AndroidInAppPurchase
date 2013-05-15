398ca4502f38eda30f995ea1de8bee0117f9946f
pozirk <pozirk@gmail.com>
2013-05-15 15:18:07 -0400

    default platform (didn't test)

--- build.bat
+++ build.bat
@@ -8,4 +8,7 @@ rd android\InAppPurchase\bin\classes /S /Q
 
 "c:\Program Files (x86)\Java\jdk1.7.0_13\bin\jar.exe" cvf android/InAppPurchase/build/libInAppPurchase.jar -C android/InAppPurchase/bin .
 
-"c:\Program Files (x86)\Adobe\AIR3.7\bin\adt.bat" -package -target ane InAppPurchase.ane air/extension.xml -swc air/InAppPurchase/bin/InAppPurchase.swc -platform Android-ARM -C android/InAppPurchase/build/ .
+SET PLATFORM_ANDROID= -platform Android-ARM -C android\InAppPurchase\build\ .
+SET PLATFORM_DEFAULT= -platform default -C default\ .
+
+"c:\Program Files (x86)\Adobe\AIR3.7\bin\adt.bat" -package -target ane InAppPurchase.ane air\extension.xml -swc air/InAppPurchase/bin/InAppPurchase.swc %PLATFORM_ANDROID% %PLATFORM_DEFAULT%
